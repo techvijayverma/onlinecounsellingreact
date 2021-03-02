d5081b1012563cf44456f0b030275591bd6858e9
vijay <vijayvermakumar@gmail.com>
2021-03-02 09:12:06 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header(){
+    return(
+        <div>
+            <h1> Header </h1>
+            <h2>This is the custom component</h2>
+
+        </div>
+
+
+    );
+}
+export default Header;
